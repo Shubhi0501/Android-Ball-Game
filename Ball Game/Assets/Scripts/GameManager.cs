e74c99e3129daef1a442a6a32f8c3680843af008
Deepali Agarwal <deepali@beautiful.ai>
2020-06-19 20:11:14 -0700

    uploading ball game

---
+++ Ball Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject WinText;
+    public GameObject RestartButton;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScoreUp()
+    {
+        score++;
+        if(score >= 5)
+        {
+            Win();
+        }
+    }
+    void Win()
+    {
+
+        WinText.SetActive(true);
+
+        RestartButton.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
